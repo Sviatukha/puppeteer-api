14ea0fdccb0a069d55bf2a2dc857a42a35ea561e
Maxim Sviatukha <sviatukha.m@scrumlaunch.com>
2024-06-06 19:01:49 +0300

    commit

--- launchBrowser.mjs
+++ launchBrowser.mjs
@@ -10,6 +10,7 @@ export const handler = async (preset, fonts) => {
 
   try {
     browser = await puppeteer.launch({
+      executablePath: '/opt/render/.cache/puppeteer/chrome',
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
